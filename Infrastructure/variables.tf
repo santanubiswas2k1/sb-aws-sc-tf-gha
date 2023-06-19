6ef7e762483f10776c358453d06f19feac734800
santanubiswas2k1 <santanubiswas2k@gmail.com>
2023-06-19 17:55:33 +0100

    added workflow

--- Infrastructure/variables.tf
+++ Infrastructure/variables.tf
@@ -1,17 +1,17 @@
-variable "AWS_BUCKET_NAME" {
-  description = "The URL of the template"
-  type        = string
-  default = "sb-ss-tfstate"
-}
+# variable "AWS_BUCKET_NAME" {
+#   description = "The URL of the template"
+#   type        = string
+#   default = "sb-ss-tfstate"
+# }
 
-variable "AWS_BUCKET_KEY_NAME" {
-  description = "The URL of the template"
-  type        = string
-  default = "product-portfolio"
-}
+# variable "AWS_BUCKET_KEY_NAME" {
+#   description = "The URL of the template"
+#   type        = string
+#   default = "product-portfolio"
+# }
 
-variable "AWS_REGION" {
-  description = "The URL of the template"
-  type        = string
-  default = "eu-west-2"
-}
+# variable "AWS_REGION" {
+#   description = "The URL of the template"
+#   type        = string
+#   default = "eu-west-2"
+# }
