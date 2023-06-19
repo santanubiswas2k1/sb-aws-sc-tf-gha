39e3b2ac8d2996d499f9f8052ef9f35c2d94d07e
santanubiswas2k1 <santanubiswas2k@gmail.com>
2023-06-19 19:23:25 +0100

    added new simple test code

--- Infrastructure/provider.tf
+++ Infrastructure/provider.tf
@@ -1,8 +1,8 @@
-terraform {
-  backend "s3" {
-    bucket = var.AWS_BUCKET_NAME
-    key    = var.AWS_BUCKET_KEY_NAME
-    region = var.AWS_REGION
-    # dynamodb_table = "mytable"  # Uncomment if you want to enable state locking and consistency checking
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = var.AWS_BUCKET_NAME
+#     key    = var.AWS_BUCKET_KEY_NAME
+#     region = var.AWS_REGION
+#     # dynamodb_table = "mytable"  # Uncomment if you want to enable state locking and consistency checking
+#   }
+# }
