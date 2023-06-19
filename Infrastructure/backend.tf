a8a407fd72b8d67ba96e7bcd98f07acade343c2f
santanubiswas2k1 <santanubiswas2k@gmail.com>
2023-06-19 18:01:55 +0100

    added workflow

--- Infrastructure/backend.tf
+++ Infrastructure/backend.tf
@@ -1,7 +1,7 @@
-# terraform {
-#   backend "s3" {
-#     bucket = var.AWS_BUCKET_NAME
-#     key    = var.AWS_BUCKET_KEY_NAME
-#     region = var.AWS_REGION
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = var.AWS_BUCKET_NAME
+    key    = var.AWS_BUCKET_KEY_NAME
+    region = var.AWS_REGION
+  }
+}
